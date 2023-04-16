fed5e71d26ce2db1fd91746c9bcbf0238f9929d8
RenanBelo <71426661+RenanBelo@users.noreply.github.com>
2023-04-16 07:18:53 -0300

    Update testando_a_api.py

--- api_teste/testando_a_api.py
+++ api_teste/testando_a_api.py
@@ -15,3 +15,8 @@ if response.status_code == 200:
         print(restaurante['nome'], restaurante['media_sentimento'], restaurante['avaliacao'], restaurante['endereco'])
 else:
     print('Erro ao obter recomendações:', response.status_code)
+    
+    #Nesse exemplo, estamos enviando um feedback negativo sobre um restaurante e esperamos receber uma lista de recomendações de restaurantes com base, 
+    #nas avaliações que contenham a palavra "horrível" ou "ruim".
+
+    #O resultado da requisição será impresso no console, mostrando a mensagem de recomendação e os dados dos 10 restaurantes mais recomendados.
