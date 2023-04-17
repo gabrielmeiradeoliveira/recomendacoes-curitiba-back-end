f99e730d8633667f2889f5c61608b69f66e63de2
RenanBelo <71426661+RenanBelo@users.noreply.github.com>
2023-04-17 12:34:21 -0300

    Update api_teste.py

--- api_teste/api_teste.py
+++ api_teste/api_teste.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
 import json
+import re
 import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
 from nltk.corpus import stopwords
+
 
 nltk.download('punkt')
 
@@ -31,42 +33,61 @@ stopwords_pt = set(stopwords.words('portuguese'))
 
 app = Flask(__name__)
 
-@app.route('/recomendar', methods=['POST'])
+@app.route('/recomendar', methods=['GET'])
 def recomendar():
-    # Receber a entrada do usuário em formato de JSON
-    feedback = request.get_json()['feedback']
-    
+    # Receber a entrada do usuário em formato de JSONP
+    callback = request.args.get('callback')
+    feedback = request.args.get('feedback')
+
     # Realizando análise de sentimentos da entrada do usuário
     sentimento = sia.polarity_scores(feedback)['compound']
+
+     # Transformar a entrada do usuário em uma expressão regular para pesquisa de frase completa
+    feedback_regex = re.escape(feedback)
+
 
     # Recomendação de restaurantes
     recomendados = []
     for restaurante in data:
-        # Análise de sentimento das avaliações que contêm a palavra ou frase digitada pelo usuário
+        # Análise de sentimento das avaliações que contêm a frase digitada pelo usuário
         avaliacoes = restaurante["reviews"]
-        sentimento_avaliacoes = [sia.polarity_scores(review["content"])["compound"] for review in avaliacoes if feedback in review["content"].lower() or any(palavra in review["content"].lower() for palavra in feedback.split())]
+        sentimento_avaliacoes = [sia.polarity_scores(review["content"])["compound"] for review in avaliacoes if re.search(feedback_regex, review["content"].lower())]
         if sentimento_avaliacoes:
-            # Se houver pelo menos uma avaliação com a palavra ou frase digitada, calcular a média de sentimentos
+            # Se houver pelo menos uma avaliação com a frase digitada, calcular a média de sentimentos
             media_sentimento = sum(sentimento_avaliacoes) / len(sentimento_avaliacoes)
             # Adicionar o restaurante à lista de recomendados com base na média de sentimentos
             recomendados.append((restaurante, media_sentimento))
-
-    # Classificando a lista de restaurantes recomendados com base na média de sentimentos
-    if recomendados:
-        # Classificando os restaurantes de acordo com o sentimento da entrada do usuário
-        if sentimento > 0:
-            # Para entradas positivas, classificar em ordem decrescente de média de sentimentos
-            recomendados = sorted(recomendados, key=lambda r: r[1], reverse=True)
-        elif sentimento < 0:
-            # Para entradas negativas, classificar em ordem crescente de média de sentimentos
-            recomendados = sorted(recomendados, key=lambda r: r[1])
-        else:
-            # Para entradas neutras, não classificar
-            pass
-        # Retornando os top 10 restaurantes recomendados
-        return {"message": "Recomendamos os seguintes restaurantes:", "restaurantes": [{"nome": restaurante["name"], "media_sentimento": media_sentimento, "avaliacao": restaurante["average_rating"], "endereco": restaurante["address"]} for restaurante, media_sentimento in recomendados[:10]]}
-    else:
-        return {"message": "Não encontramos nenhum restaurante que atenda às suas preferências. " }
     
+     # Classificando a lista de restaurantes recomendados com base na média de sentimentos
+    if recomendados:
+        media_sentimentos = sum([r[1] for r in recomendados])/len(recomendados)
+        recomendados = [(r[0], r[1], sentimento - r[1]) for r in recomendados]
+        recomendados = sorted(recomendados, key=lambda r: r[2])
+        recomendados = [(r[0], r[1]) for r in recomendados]
+
+    # Classificando os restaurantes de acordo com o sentimento da entrada do usuário
+    if sentimento > 0:
+        # Para entradas positivas, classificar em ordem decrescente de média de sentimentos
+        recomendados = sorted(recomendados, key=lambda r: r[1], reverse=True)
+    elif sentimento < 0:
+        # Para entradas negativas, classificar em ordem crescente de média de sentimentos
+        recomendados = sorted(recomendados, key=lambda r: r[1])
+    else:
+        # Caso o sentimento seja neutro, manter a ordem original da lista
+        pass
+
+    # Retornando os top 10 restaurantes recomendados
+    top10_recomendados = recomendados[:10]
+
+    # Criar um objeto JSON com os restaurantes recomendados
+    data_json = json.dumps(top10_recomendados)
+
+    # Criar a resposta JSONP, adicionando a função de callback ao início do objeto JSON
+    response = callback + '(' + data_json + ')'
+
+    # Retornar a resposta JSONP
+    return response
+
 if __name__ == '__main__':
     app.run()
+
